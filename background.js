a045b66e411b69c9895fa0eac06ede6b8ac03dc5
Shih-Ming Wang <swang150@ucsc.edu>
2023-12-22 21:58:32 -0800

    Regex match urls & replaceExistingTab even when tab is loading to speed up.

--- background.js
+++ background.js
@@ -1,35 +1,61 @@
 chrome.webNavigation.onBeforeNavigate.addListener((details) => {
   const { url, tabId } = details;
-  jumpToExistingTab(url, tabId);
+  replaceExistingTab(url, tabId);
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete") {
-    replaceExistingTab(tab.url, tab.id);
-  }
+chrome.tabs.onUpdated.addListener((_tabId, _changeInfo, tab) => {
+  replaceExistingTab(tab.url, tab.id);
 });
+
+async function isCurrentTab(tabId) {
+  let [currentTab] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  return currentTab.id == tabId;
+}
 
 async function replaceExistingTab(url, tabId) {
+  if (url == "") {
+    return;
+  }
+
+  // Sanity check to only remove tabId once.
+  let isCurrent = await isCurrentTab(tabId);
+  if (!isCurrent) {
+    return;
+  }
+
   let tabs = await chrome.tabs.query({});
-  tabs.forEach((other) => {
-    if (other.url == url && other.id !== tabId) {
+  tabs.some((other) => {
+    if (other.id !== tabId && areUrlSimiliar(other.url, url)) {
       chrome.tabs.update(other.id, { active: true });
       chrome.tabs.remove(tabId);
+      return true;
     }
   });
 }
 
-async function jumpToExistingTab(url, tabId) {
-  if (url == "") {
-    return;
-  }
-  let tabs = await chrome.tabs.query({ url: url });
+var urlRegex = [RegExp("https://docs.google.com/document/d/([^/]*)")];
 
-  if (tabs != null && tabs.length > 1) {
-    const existingTab = tabs.find((t) => t.id !== tabId);
-    if (existingTab) {
-      chrome.tabs.update(existingTab.id, { active: true });
-      chrome.tabs.remove(tabId);
+function areUrlSimiliar(url1, url2) {
+  for (let i = 0; i < urlRegex.length; ++i) {
+    let r = urlRegex[i];
+    let m1 = url1.match(r);
+    let m2 = url2.match(r);
+    if (m1 == null || m2 == null || m1.length != m2.length) {
+      continue;
+    }
+    let allGroupMatch = true;
+    for (let j = 1; j < m1.length; ++j) {
+      if (m1[j] != m2[j]) {
+        allGroupMatch = false;
+        break;
+      }
+    }
+    if (allGroupMatch) {
+      return true;
     }
   }
+  return url1 == url2;
 }
