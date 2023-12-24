6902345934840e5dcd54bff38320c44386b2dd37
Shih-Ming Wang <swang150@ucsc.edu>
2023-12-23 18:47:37 -0800

    lint.

--- background.js
+++ background.js
@@ -17,31 +17,31 @@ chrome.tabs.onUpdated.addListener((_tabId, _changeInfo, tab) => {
 });
 
 async function isCurrentTab(tabId) {
-  let [currentTab] = await chrome.tabs.query({
+  const [currentTab] = await chrome.tabs.query({
     active: true,
     lastFocusedWindow: true,
   });
-  return currentTab.id == tabId;
+  return currentTab.id === tabId;
 }
 
 async function replaceExistingTab(url, tabId) {
-  if (url == "") {
+  if (url === "") {
     return;
   }
 
   // Sanity check to only remove tabId once.
-  let isCurrent = await isCurrentTab(tabId);
+  const isCurrent = await isCurrentTab(tabId);
   if (!isCurrent) {
     return;
   }
 
-  let tabs = await chrome.tabs.query({});
-  let data = await chrome.storage.sync.get("urlPattern");
+  const tabs = await chrome.tabs.query({});
+  const data = await chrome.storage.sync.get("urlPattern");
   const urlPattern = (data.urlPattern || []).map((s) => {
     return RegExp(s);
   });
   tabs.some((other) => {
-    if (other.id !== tabId && areUrlSimiliar(other.url, url, urlPattern)) {
+    if (other.id !== tabId && areUrlSimilar(other.url, url, urlPattern)) {
       chrome.tabs.update(other.id, { active: true });
       chrome.tabs.remove(tabId);
       return true;
@@ -49,17 +49,16 @@ async function replaceExistingTab(url, tabId) {
   });
 }
 
-function areUrlSimiliar(url1, url2, urlPatterns) {
-  for (let i = 0; i < urlPatterns.length; ++i) {
-    let r = urlPatterns[i];
-    let m1 = url1.match(r);
-    let m2 = url2.match(r);
-    if (m1 == null || m2 == null || m1.length != m2.length) {
+function areUrlSimilar(url1, url2, urlPatterns) {
+  for (const r of urlPatterns) {
+    const m1 = url1.match(r);
+    const m2 = url2.match(r);
+    if (m1 === null || m2 === null || m1.length !== m2.length) {
       continue;
     }
     let allGroupMatch = true;
     for (let j = 1; j < m1.length; ++j) {
-      if (m1[j] != m2[j]) {
+      if (m1[j] !== m2[j]) {
         allGroupMatch = false;
         break;
       }
@@ -68,5 +67,5 @@ function areUrlSimiliar(url1, url2, urlPatterns) {
       return true;
     }
   }
-  return url1 == url2;
+  return url1 === url2;
 }
